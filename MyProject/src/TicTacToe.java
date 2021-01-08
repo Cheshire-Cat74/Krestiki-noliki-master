8a8bb76b7e1c44d8620a4c7dced2998552de3e62
Cheshire-Cat74 <75084362+Cheshire-Cat74@users.noreply.github.com>
2021-01-08 11:15:11 +0500

    Add files via upload

---
+++ MyProject/src/TicTacToe.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TicTacToe extends Application {
+    @Override public void start(Stage stage) throws Exception {
+        GameManager gameManager = new GameManager();
+
+        Scene scene = gameManager.getGameScene();
+        scene.getStylesheets().add(
+                getResource(
+                        "tictactoe1.css"
+                )
+        );
+
+        stage.setTitle("Tic-Tac-Toe");
+        stage.getIcons().add(SquareSkin.crossImage);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private String getResource(String resourceName) {
+        return getClass().getResource(resourceName).toExternalForm();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(TicTacToe.class);
+    }
+}
+
+
